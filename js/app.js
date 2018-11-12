fb318d55d2dd9a85e443c92b6f8d64f1f927cde3
imidaho <jamesmcochran@gmail.com>
2018-11-12 17:37:08 -0500

    added form and java script function, some aesthetic changes

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+function openForm() {
+    var form = document.getElementById("myForm");
+    if (form.style.display ==="none") {
+        form.style.display ="block";
+    } else { 
+        form.style.display ="none";
+    }
+  }
+  
+
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  };
+
+
